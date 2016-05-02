2f002c5b190a14cf9ea6c402c21dad808067614c
daniellelara <daniellelaragourgey@gmail.com>
2016-05-02 16:25:31 +0100

    index set up

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var app = express();
 var router = require('./config/routes');
 var config = require('./config/app');
 
-express.static(__dirname + "/public");
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/public/views");
+app.use(express.static(__dirname + '/public'));
 
 mongoose.connect(config.databaseUrl);
 
@@ -17,7 +19,9 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api', router);
-
+app.get('/', function(req, res) {
+  res.render('index');
+});
 app.listen(config.port, function() {
   console.log("Express is listening on port " + config.port);
 });
